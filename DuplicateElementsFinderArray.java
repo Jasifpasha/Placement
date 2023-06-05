89546349cd693faa4c7bbdcb16d943e5f3b37430
Shaik Jasif Pasha <jasifpasha2000@gmail.com>
2023-06-05 15:00:47 +0530

    Update DuplicateElementsFinderArray.java
    Optimized.

--- DuplicateElementsFinderArray.java
+++ DuplicateElementsFinderArray.java
@@ -1,16 +1,24 @@
+import java.util.*;
 public class DuplicateElementsFinderArray {
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 1, 2, 5, 6, 3, 7, 8, 9, 9, 7};
+            int[] arr = {1, 2, 3, 4, 1, 2, 5, 6, 3, 7, 8, 9, 9, 7};
+            findDuplicates(arr);
+        }
 
-        System.out.print("Duplicate elements in the array: ");
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == array[j]) {
-                    System.out.print(array[i] + " ");
-                    break;
+        public static void findDuplicates(int[] arr) {
+            Set<Integer> uniqueElements = new HashSet<>();
+            Set<Integer> duplicateElements = new HashSet<>();
+
+            for (int i = 0; i < arr.length; i++) {
+                if (!uniqueElements.add(arr[i])) {
+                    duplicateElements.add(arr[i]);
                 }
             }
-        }
+
+            System.out.print("Duplicate elements in the array: ");
+            for (int element : duplicateElements) {
+                System.out.print(element + " ");
+            }
     }
 }
